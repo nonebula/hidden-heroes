28c866ac058772ad9d8ea95eba335e85e9a34cf2
Joe Lloyd Higgs <joe@Joes-MacBook-Air.local>
2024-05-02 10:49:10 +0100

    Map component working on page

--- src/components/MapComponents/Map.jsx
+++ src/components/MapComponents/Map.jsx
@@ -1,19 +1,20 @@
-import { MapContainer, TileLayer, useMap, Marker, Popup } from "react-leaflet";
+import React from "react";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 
-const Map = () => {
+function Map() {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+    <MapContainer
+      className="mb-16"
+      center={[51.505, -0.09]}
+      zoom={13}
+      style={{ height: "400px", width: "100%" }}
+    >
       <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
-      <Marker position={[51.505, -0.09]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker>
     </MapContainer>
   );
-};
+}
 
 export default Map;
--- src/pages/Discover/Discover.jsx
+++ src/pages/Discover/Discover.jsx
@@ -1,8 +1,16 @@
 import Map from "../../components/MapComponents/Map";
 const Discover = () => {
   return (
-    <div className="bg-black border border-lime-500 px-16 mx-16 flex flex-col items-center font-roboto-mono">
-      <h1 className="text-lime-700 font-bold mt-5 mb-5">Discover</h1>
+    <div className="bg-black border border-lime-500 px-16 mx-16 pb-20 flex flex-col items-center font-roboto-mono">
+      <h1 className="text-lime-700 text-4xl font-bold mt-5 mb-10">Discover</h1>
+      <p className="text-lime-500 mb-10">
+        There have been reports of a abnormally lucky experiences in London,
+        trending on social media. Our intelligence suggests there may be a
+        special in the area. Click around to inspect different spots and see if
+        you have any luck. Once you are done with London, get out there and see
+        who else you can find. We cannot miss this opportunity to locate these
+        specials!{" "}
+      </p>
       <Map />
     </div>
   );
